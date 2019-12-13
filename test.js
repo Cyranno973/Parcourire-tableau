10e6347266f63482b5bd54d2f92e1e1671782e62
Cyranno973 <gs.gouin@gmail.com>
2019-12-13 22:54:16 +0100

    fin du test

---
+++ test.js
@@ -0,0 +1,55 @@
+var x = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+// var multiplication = chiffres.map(function(num){
+// 	return num * 2 ;
+// });
+
+// console.log(multiplication);
+
+// for (var i=0;i<x.length;i++)
+// {
+// 	console.log(x[i]);	
+// }
+
+// for (var i in x)
+// {
+// 	console.log(x[i]);
+	
+// }
+
+
+
+
+// function mult(arr){
+// 	for(var i=0; i<arr.length;i++){
+// 		arr[i] *= 2  ;
+// 	}
+// 	return arr
+// }
+// console.log(mult(x));
+
+
+// function mult(arr){
+// 	for (var i in arr)
+// 	{
+// 		 arr[i] *=2;
+// 	}
+// 	return arr
+// }
+// console.log(mult(x));
+
+// x.forEach(function(arr){
+//  console.log(arr);
+// });
+
+
+// x.forEach((arr) =>{
+// 	console.log(arr);
+//    });
+
+// x.map((arr =>{
+// 	console.log(arr);	
+// }));
+
+x.map(arr => console.log(arr));
+
